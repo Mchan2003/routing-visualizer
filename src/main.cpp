5a770a8059ac478f53c5098eae55ae2ce20ce279
Michael Chan <mchan100work@gmail.com>
2024-05-01 00:23:12 -0700

    sfml and node files setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,34 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    sf::CircleShape shape1(50.f);
+    shape1.setFillColor(sf::Color::Green);
+    shape1.setPosition(100,100);
+
+    sf::CircleShape shape2(50.f);
+    shape2.setFillColor(sf::Color::Green);
+    shape2.setPosition(200,400);
+
+    sf::CircleShape shape3(50.f);
+    shape3.setFillColor(sf::Color::Green);
+    shape3.setPosition(300,200);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(shape1);
+        window.draw(shape2);
+        window.draw(shape3);
         window.display();
     }
+
+    return 0;
 }
