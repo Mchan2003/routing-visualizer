f4aefdfee8e0330c47d0a63f8a7c4cf34739bab3
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2024-05-02 12:05:08 -0700

    Add files via upload

--- src/node.cpp
+++ src/node.cpp
@@ -5,54 +5,49 @@
 #include <utility>
 
 uint32_t distance_function(std::pair<uint32_t, uint32_t> p1,
-                           std::pair<uint32_t, uint32_t> p2)
-{
-  int a = (int)abs((int)p1.first - (int)p2.first);
-  int b = (int)abs((int)p2.second - (int)p2.second);
-  return std::hypot(a, b);
+                           std::pair<uint32_t, uint32_t> p2) {
+    int a = (int)abs((int)p1.first - (int)p2.first);
+    int b = (int)abs((int)p2.second - (int)p2.second);
+    return std::hypot(a, b);
 }
 
-void Node::add_edge(char dest, int p_weight)
-{
-  // Pull ptr from map, both dest and source, assume all nodes exist
-  std::shared_ptr<Node> dest_ptr;
-  try {
-    dest_ptr = all_nodes.at(dest);
-  } catch (...) {
-    printf("Node doesn't exist, please create it first!\n");
-    throw std::out_of_range("Node doesn't exist, please create it first!");
-  }
-  if (dest_ptr.get() == nullptr) {
-    throw std::invalid_argument("some how dest_ptr was not set");
-  }
-  auto source_ptr = all_nodes.at(label);
-  int weight;
-  if (p_weight == 0) {
-    weight = distance_function(source_ptr->coords, dest_ptr->coords);
-  } else {
-    weight = p_weight;
-  }
-  source_ptr->adj.push_back(std::make_pair(dest_ptr, weight));
-  dest_ptr->adj.push_back(std::make_pair(source_ptr, weight));
+void Node::add_edge(char dest, int p_weight) {
+    // Pull ptr from map, both dest and source, assume all nodes exist
+    std::shared_ptr<Node> dest_ptr;
+    try {
+        dest_ptr = all_nodes.at(dest);
+    } catch (...) {
+        printf("Node doesn't exist, please create it first!\n");
+        throw std::out_of_range("Node doesn't exist, please create it first!");
+    }
+    if (dest_ptr.get() == nullptr) {
+        throw std::invalid_argument("some how dest_ptr was not set");
+    }
+    auto source_ptr = all_nodes.at(label);
+    int weight;
+    if (p_weight == 0) {
+        weight = distance_function(source_ptr->coords, dest_ptr->coords);
+    } else {
+        weight = p_weight;
+    }
+    source_ptr->adj.push_back(std::make_pair(dest_ptr, weight));
+    dest_ptr->adj.push_back(std::make_pair(source_ptr, weight));
 }
 
-void Node::remove_edge(char dest)
-{
-  auto dest_ptr = all_nodes.at(dest);
-  adj.erase(std::find_if(adj.begin(), adj.end(), [&dest_ptr](const auto& p) {
-    return p.first == dest_ptr;
-  }));
-  auto source_ptr = all_nodes.at(label);
-
-  dest_ptr->adj.erase(std::find_if(
-    dest_ptr->adj.begin(), dest_ptr->adj.end(), [&](const auto& p) {
-      return p.first == dest_ptr;
+void Node::remove_edge(char dest) {
+    auto dest_ptr = all_nodes.at(dest);
+    adj.erase(std::find_if(adj.begin(), adj.end(), [&dest_ptr](const auto &p) {
+        return p.first == dest_ptr;
     }));
+    auto source_ptr = all_nodes.at(label);
 
-  if (dest_ptr.use_count() == 1) {
-    all_nodes.erase(
-      std::find_if(all_nodes.begin(), all_nodes.end(), [&](const auto& p) {
-        return p.second == dest_ptr;
-      }));
-  }
+    dest_ptr->adj.erase(
+            std::find_if(dest_ptr->adj.begin(), dest_ptr->adj.end(),
+                         [&](const auto &p) { return p.first == dest_ptr; }));
+
+    if (dest_ptr.use_count() == 1) {
+        all_nodes.erase(
+                std::find_if(all_nodes.begin(), all_nodes.end(),
+                             [&](const auto &p) { return p.second == dest_ptr; }));
+    }
 }
--- src/node.h
+++ src/node.h
@@ -9,52 +9,49 @@
 
 constexpr uint32_t COORD_MAX = 900;
 
-struct Node
-{
-  // Change to map, letter is key
+struct Node {
+    // Change to map, letter is key
 
-  /// @brief Used to store all nodes at any given time, each is mapped to a
-  /// letter
-  inline static std::unordered_map<char, std::shared_ptr<Node>> all_nodes;
+    /// @brief Used to store all nodes at any given time, each is mapped to a
+    /// letter
+    inline static std::unordered_map<char, std::shared_ptr<Node>> all_nodes;
 
-  /// @brief the label of a node (eg node A, B what ever)
-  char label;
+    /// @brief the label of a node (eg node A, B what ever)
+    char label;
 
-  /// @brief The nodes this node is connected too, this graph is not
-  /// directional. (each edge is represented as a bidirectional edge), dont
-  /// write to this directly, use the helper methods
-  std::vector<std::pair<std::shared_ptr<Node>, int>> adj;
-  std::pair<uint32_t, uint32_t> coords;
-  static std::shared_ptr<Node> create(char p_label,
-                                      std::pair<uint32_t, uint32_t> p_coords)
-  {
-    auto new_node = std::make_shared<Node>(Node(p_label));
-    if (p_coords.first > COORD_MAX || p_coords.second > COORD_MAX) {
-      throw std::out_of_range("coords out of range!");
+    /// @brief The nodes this node is connected too, this graph is not
+    /// directional. (each edge is represented as a bidirectional edge), dont
+    /// write to this directly, use the helper methods
+    std::vector<std::pair<std::shared_ptr<Node>, int>> adj;
+    std::pair<uint32_t, uint32_t> coords;
+    static std::shared_ptr<Node> create(char p_label,
+                                        std::pair<uint32_t, uint32_t> p_coords) {
+        auto new_node = std::make_shared<Node>(Node(p_label));
+        if (p_coords.first > COORD_MAX || p_coords.second > COORD_MAX) {
+            throw std::out_of_range("coords out of range!");
+        }
+        new_node->coords = p_coords;
+        new_node->label = p_label;
+        all_nodes[p_label] = new_node;
+        return new_node;
     }
-    new_node->coords = p_coords;
-    new_node->label = p_label;
-    all_nodes[p_label] = new_node;
-    return new_node;
-  }
 
-  /// @brief Adds an edge to the node. (Edges are represented as two directional
-  /// edges from source to dest and dest to source), source being this
-  /// @param dest The destination node to add to, creates the node if it doesn't
-  /// exist
-  /// @param weight The weight of this edge
-  /// @throws invalid_argument if the destination ptr was not set, should never
-  /// actually happen.
-  void add_edge(char dest, int p_weight = 0);
+    /// @brief Adds an edge to the node. (Edges are represented as two directional
+    /// edges from source to dest and dest to source), source being this
+    /// @param dest The destination node to add to, creates the node if it doesn't
+    /// exist
+    /// @param weight The weight of this edge
+    /// @throws invalid_argument if the destination ptr was not set, should never
+    /// actually happen.
+    void add_edge(char dest, int p_weight = 0);
 
-  /// @brief Removes an edge
-  /// @param dest The node to remove from
-  void remove_edge(char dest);
+    /// @brief Removes an edge
+    /// @param dest The node to remove from
+    void remove_edge(char dest);
 
 private:
-  Node() = delete;
-  Node(char p_label)
-    : label(p_label){};
+    Node() = delete;
+    Node(char p_label) : label(p_label) {};
 };
 
 using node_ptr = std::shared_ptr<Node>;
